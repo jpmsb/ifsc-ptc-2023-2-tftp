a0bf1c7693ecbda1ce40f3c236c15f7a7deb0b45
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-10-14 17:00:08 -0300

    Classe para criar o pacote DATA

---
+++ servidor/DATA.cpp
@@ -0,0 +1,70 @@
+#include "DATA.h"
+
+using namespace std;
+
+DATA::DATA()
+  : opcode(htons(3)), block(0), count(0), bytesAmount(0)
+{
+    
+}
+
+DATA::DATA(string & filename)
+  : opcode(htons(3)), block(htons(1)), bytesAmount(0), file(filename, ios::binary)
+{
+    file.read((char *)data, 512);
+    bytesAmount = file.gcount();
+    count = ntohs(block);
+    
+}
+
+DATA::DATA(char bytes[], size_t size){
+    uint8_t uint16Size = sizeof(uint16_t);
+
+    memcpy(&opcode, bytes, uint16Size);
+    memcpy(&block, bytes + uint16Size, uint16Size);
+    memcpy(data, bytes+4, size-4);
+    bytesAmount = size-4;
+    count = ntohs(block);
+}
+
+void DATA::increment(){
+    count = ntohs(block)+1;
+    block = htons(count);
+
+    file.read((char *)data, 512);
+    bytesAmount = file.gcount();
+}
+
+uint16_t DATA::getBlock(){
+    return count;
+}
+
+uint16_t DATA::getOpcode(){
+    return ntohs(opcode);
+}
+
+const char* DATA::getData(){
+    return data;
+}
+
+int DATA::size(){
+    return 4+bytesAmount; 
+}
+
+int DATA::dataSize(){
+    return bytesAmount;
+}
+
+void DATA::setData(char _data[], size_t size){
+    memcpy(data, _data, size);
+    bytesAmount = size;
+}
+
+void DATA::setBytes(char bytes[], size_t size){
+    uint8_t uint16Size = sizeof(uint16_t);
+
+    memcpy(&opcode, bytes, uint16Size);
+    memcpy(&block, bytes + uint16Size, uint16Size);
+    memcpy(data, bytes+4, size-4);
+    bytesAmount = size-4;
+}
---
+++ servidor/DATA.h
@@ -0,0 +1,35 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <cstdint>
+#include <arpa/inet.h>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+class DATA {
+    public:
+        DATA();
+        DATA(std::string & filename);
+        DATA(char bytes[], size_t size);
+
+        uint16_t getBlock();
+        uint16_t getOpcode();
+        const char* getData();
+        void increment();
+        void setData(char _data[], size_t size);
+        int size();
+        int dataSize();
+        void setBytes(char bytes[], size_t size);
+
+    private:
+        uint16_t opcode;
+        uint16_t block;
+        char data[512];
+        int count;
+	std::ifstream file;
+        std::streamsize bytesAmount;
+};
+
+#endif
