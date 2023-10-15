62e17322fcc88112a53be18ae481b5c4201d11eb
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-10-15 20:05:24 -0300

    Programa de testes do servidor TFTP

---
+++ servidor/servidor.cpp
@@ -0,0 +1,26 @@
+#include "poller.h"
+#include "UDPSocket.h"
+#include <iostream>
+#include <string>
+#include "TFTPServer.h"
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    int porta = stoi(argv[1]);
+    string diretorio_raiz = argv[2];
+
+    Poller sched;
+
+    sockpp::AddrInfo addr(porta);
+    sockpp::UDPSocket sock(addr);
+
+    // Cria um objeto TFTP com timeout de 3 segundos
+    TFTPServer * cb_tftp = new TFTPServer(sock, addr, 3, diretorio_raiz);
+
+    // Adiciona o objeto TFTP ao poller
+    sched.adiciona(cb_tftp);
+    sched.despache();
+
+    return 0;
+}
